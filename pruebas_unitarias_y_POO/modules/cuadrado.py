10e66e5458332993d81b26898a0274b3292267b4
jrizz <juan.rizzato@uner.edu.ar>
2025-03-18 12:53:46 -0300

    modifico if __name__=='__main__' de cuadrado

--- pruebas_unitarias_y_POO/modules/cuadrado.py
+++ pruebas_unitarias_y_POO/modules/cuadrado.py
@@ -26,22 +26,22 @@ class Cuadrado:
         # Calcula y devuelve el perímetro del cuadrado (4 * lado).
         return 4 * self.lado
     
-# cuadrado1 = Cuadrado(5) # crea un cuadrado de lado 5
-# print(cuadrado1.area()) # lado al cuadrado
-# print(cuadrado1.perimetro()) # lado * 4
-# cuadrado1.set_lado(10) # cambia el lado a 10
-# print(cuadrado1.area()) # lado al cuadrado
-# print(cuadrado1.perimetro()) # lado * 4
-# cuadrado1.set_lado(-5) # lanza una excepción
-# cuadrado2 = Cuadrado(-5) # lanza una excepción
+cuadrado1 = Cuadrado(5) # crea un cuadrado de lado 5
+print(cuadrado1.area()) # lado al cuadrado
+print(cuadrado1.perimetro()) # lado * 4
+cuadrado1.set_lado(10) # cambia el lado a 10
+print(cuadrado1.area()) # lado al cuadrado
+print(cuadrado1.perimetro()) # lado * 4
+cuadrado1.set_lado(-5) # lanza una excepción
+cuadrado2 = Cuadrado(-5) # lanza una excepción
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    cuadrado1 = Cuadrado(5) # crea un cuadrado de lado 5
-    print(cuadrado1.area()) # lado al cuadrado
-    print(cuadrado1.perimetro()) # lado * 4
-    cuadrado1.set_lado(10) # cambia el lado a 10
-    print(cuadrado1.area()) # lado al cuadrado
-    print(cuadrado1.perimetro()) # lado * 4
-    cuadrado1.set_lado(-5) # lanza una excepción
-    cuadrado2 = Cuadrado(-5) # lanza una excepción
+#     cuadrado1 = Cuadrado(5) # crea un cuadrado de lado 5
+#     print(cuadrado1.area()) # lado al cuadrado
+#     print(cuadrado1.perimetro()) # lado * 4
+#     cuadrado1.set_lado(10) # cambia el lado a 10
+#     print(cuadrado1.area()) # lado al cuadrado
+#     print(cuadrado1.perimetro()) # lado * 4
+#     cuadrado1.set_lado(-5) # lanza una excepción
+#     cuadrado2 = Cuadrado(-5) # lanza una excepción
