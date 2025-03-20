27541af73887e129ea0d3f88d1ea210a1318ba95
chiaragk22 <chiarigk@gmail.com>
2025-03-20 00:37:43 -0300

    modificaciones post clase

--- pruebas_unitarias_y_POO/modules/piramide.py
+++ pruebas_unitarias_y_POO/modules/piramide.py
@@ -3,48 +3,6 @@ import math
 class Piramide:
     # *** Constructor: Debe validar que base_longitud y altura sean positivos, luego asignarlos a los atributos
     def __init__(self, base_longitud, altura):
-        # Escriban su código acá
-        pass
-
-    # *** Método getter base_longitud
-    @property
-    def base_longitud(self):
-        # Escriban su código acá
-        pass
-
-    # *** Método getter altura
-    @property
-    def altura(self):
-        # Escriban su código acá
-        pass
-
-    # *** Método setter base_longitud: Debe validar que el valor sea positivo
-    @base_longitud.setter
-    def base_longitud(self, nueva_base_longitud):
-        # Escriban su código acá
-        pass
-
-    # *** Método setter altura: Debe validar que el valor sea positivo
-    @altura.setter
-    def altura(self, nueva_altura):
-        # Escriban su código acá
-        pass
-    
-    # *** Método volumen: Debe calcular (base_longitud² * altura) / 3
-    def volumen(self):
-        # Escriban su código acá
-        pass
-    
-    # *** Método area_superficial: Fórmula area = base_longitud * (base_longitud + math.sqrt(4*altura² + (base_longitud/2)²))
-    def area_superficial(self):
-        # Escriban su código acá
-        pass
-
-import math
-
-class Piramide:
-    # *** Constructor: Debe validar que base_longitud y altura sean positivos, luego asignarlos a los atributos
-    def __init__(self, base_longitud, altura):
         if base_longitud <= 0 and altura <= 0:
             raise ValueError("La base debe ser positiva")
         self.__base_longitud = base_longitud
--- pruebas_unitarias_y_POO/modules/prisma_trapezoidal.py
+++ pruebas_unitarias_y_POO/modules/prisma_trapezoidal.py
@@ -1,7 +1,4 @@
 class PrismaTrapezoidal:
-    pass
-
-class PrismaTrapezoidal:
 
     def __init__(self, base_mayor, base_menor, lado, altura_trapecio, altura_prisma):
         if base_mayor <= 0 or base_menor <= 0 or lado <= 0 or altura_trapecio <= 0 or altura_prisma <= 0:
@@ -33,10 +30,17 @@ class PrismaTrapezoidal:
         return self.__altura_trapezio
 
     def volumen(self):
-        volumen = (((self.base_mayor + self.base_menor) / 2) * self.altura_trapecio) * self.altura_prisma
+        volumen = (((self.__base_mayor + self.base_menor) / 2) * self.__altura_trapecio) * self.__altura_prisma
         return volumen
     
+    def area_superficial(self):
+        area_superficial = (self.__base_mayor + self.__base_menor) * self.__altura_trapecio + (self.__base_mayor + self.__base_menor + 2 * self.__lado) * self.__altura_prisma
+        return area_superficial
 
+
+prisma1 = PrismaTrapezoidal(8, 4, 5, 3, 10)
+print(prisma1.volumen())
+print(prisma1.area_superficial())
 
 
 
